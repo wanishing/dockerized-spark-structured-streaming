12253c0ff212e82d4a2fd0024bff57eacf70f00e
twanish <tal.wanish@riskified.com>
2022-08-01 12:34:41 +0300

    no message

--- it/down.sh
+++ it/down.sh
@@ -1,3 +1,2 @@
 set -x
 docker-compose -f it/docker-compose.infra.yml down --remove-orphans
-docker-compose -f it/docker-compose.yml down --remove-orphans
--- it/up.sh
+++ it/up.sh
@@ -1,5 +1,5 @@
 set -x
-docker-compose -f it/docker-compose.infra.yml up -d
+docker-compose -f it/docker-compose.infra.yml up -d --remove-orphans
 docker wait $(docker-compose -f it/docker-compose.infra.yml ps -q init-kafka)
 docker wait $(docker-compose -f it/docker-compose.infra.yml ps -q init-s3)
 docker-compose -f it/docker-compose.yml up -d
